becf217544a68eb47a0c8783767eb24981348db1
Daniel San Martin <dsanmartins@gmail.com>
2023-11-22 11:04:21 -0300

    target folder removed

--- target/classes/META-INF/data.sql
+++
@@ -1,4 +0,0 @@
-insert into habitacion(numero,cantidad_camas, id_reserva) values(1,1,null)
-insert into habitacion(numero,cantidad_camas, id_reserva) values(2,1,null)
-insert into habitacion(numero,cantidad_camas, id_reserva) values(3,2,null)
-insert into habitacion(numero,cantidad_camas, id_reserva) values(4,2,null)
